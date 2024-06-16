bcd4a23ee0cd3de629ac0fb0967f2acef1bee810
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-06-16 19:27:49 -0400

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
 const removeFromArray = function() {
+    // converts arguments into array aside from first (given array)
+    const args = Array.prototype.slice.call(arguments, 1);
+    let filteredValues = arguments[0].filter((item) => !args.includes(item));
+    return filteredValues;
 };
 
 // Do not edit below this line
