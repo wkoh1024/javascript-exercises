8edb358fd5e7da26ad811bb2d8a2abfe6ade440b
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-06-17 17:39:59 -0400

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if (typeof firstNum !== 'number' || typeof secondNum !== 'number' 
+        || firstNum < 0 || secondNum < 0
+    ) return 'ERROR';
+    if (firstNum > secondNum) {
+        // swaps firstNum and secondNum
+        firstNum = [secondNum, secondNum = firstNum][0];
+    }
+    let answer = 0;
+    for (let i = firstNum; i <= secondNum; i++) {
+        answer += i;
+    }
+    return answer;
 };
 
 // Do not edit below this line
