ee50acdb9d9f40a7533c8934f5f089f25183c3e0
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-06-17 17:50:59 -0400

    Completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 === 0 && year % 400 === 0) return true;
+    return (year % 4 === 0 && year % 100 !== 0);
 };
 
 // Do not edit below this line
