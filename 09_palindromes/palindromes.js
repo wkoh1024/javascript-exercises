d2630045cddbc54142133624aafccab707a33f30
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-06-21 02:09:14 -0400

    Completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // matches all non word characters
+    // which means letters, numbers, and underscores (would fail if underscores used)
+    const regex = /\W/g;
+    strippedWord = word.replace(regex, '');
+    // converts string to array, reverses, and joins back
+    let reversedStrippedWord = strippedWord.split("").reverse().join("");
+    return strippedWord.toLowerCase() === reversedStrippedWord.toLowerCase();
 };
 
 // Do not edit below this line
