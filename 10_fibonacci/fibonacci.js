3bb023fe03d34426a5a515f63ed8183b4aa0a41e
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-07-16 15:25:52 -0400

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let number = Number(input);
+    if (number < 0) return 'OOPS';
+    if (number <= 1) {
+        return number;
+    }
+    return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
