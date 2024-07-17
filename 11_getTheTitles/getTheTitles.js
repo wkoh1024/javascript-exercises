cde03412006d936a1fa61dca1eccec428071a76d
Wayne <34191804+wkoh1024@users.noreply.github.com>
2024-07-17 09:54:09 -0400

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookTitles = [];
+    // book in books gives indices instead of object itself
+    for (book of books) {
+        bookTitles.push(book.title);
+    };
+    return bookTitles;
 };
 
 // Do not edit below this line
